308c2f8a265423a2f51cae31f1f54fe42fe456ec
Rass1983 <56704896+Rass1983@users.noreply.github.com>
2023-02-02 13:45:03 -0800

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
                 ensure => present,
                 user   => 'root',
                 type   => 'ssh-rsa',
-                key    => '',
+                key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDjniJnOmwpXBDBSNaXCOwayu7Cr19lBuSvaZHDMthzcbhCwOwjozmOtiSIgCRT+9H0VX03CiYGIhw+4j1tf68nbF6TwfPJTld0Xjtfgr1HvoRRTSuEZhoCyBdYxMC6K93p5EoAUhqN1cb8ydTEp6J8Yl6GAXo39W+2SvdZdLV+TLaXLmu0FI+1EnvkfbuhZu7NT8/9j8U/6Yv57jvFwzNexUavPAdTEkK+AsDWl4e+ijiNigdHun01869MFf6y7EtBelD6g5uORGDaCTZmUYVIVT4wbckaS6MzZObdeLsQg2C9QKz19lD1RN2Z/wbX/i6Pt3syMk0UdloV4c0GpwLf',
         }
 }
